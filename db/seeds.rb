71463db153701f0a5ed97f2f09cc7c54b4c97db9
shane <shane_qi@hotmail.com>
2021-11-11 13:20:37 +1000

    updating seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Amenity.create!([{name: 'bbq'}, {name: 'air-conditoning'}, {name: 'bbq'}, {name: 'bbq'}, {name: 'bbq'}, {name: 'bbq'}])
+Amenity.destroy_all
+SuburbPostcode.destroy_all
+User.destroy_all
+Profile.destroy_all
+PropertyLocation.destroy_all
+Property.destroy_all
+PropertyType.destroy_all
+Booking.destroy_all
 
 
+
+Amenity.create!([{name: 'BBQ'}, {name: 'Air-conditoning'}, {name: 'Pool'}, {name: 'Gym'}, {name: 'Stocked-kitchen'}, {name: 'Laundry'}])
+
+PropertyType.create!([{name: 'House'}, {name: 'Town House'}, {name: 'Unit'}, {name: 'Campsite'}])
+
+SuburbPostcode.create!([{postcode: 4564, suburb: 'Twin Waters', city: 'Sunshine Coast', state: 'QLD'}, {postcode: 0800, suburb: 'Darwin', city: 'Darwin', state: 'NT'}, {postcode: 3350, suburb: 'Ballarat', city: 'Geelong', state: 'VIC'}])
+
+User.create!([{email:'123@outlook.com', password:'123456'}, {email:'foo@outlook.com', password:'123456'}, {email:'vivian@outlook.com', password:'123456'}])
+
+Profile.create!(user: User.first, first_name: 'Andrew', last_name: 'Goodwill', contact_number: 23232323)
+
+PropertyLocation.create!([{street_number: 30, street_name: 'Hills street', suburb_postcode: SuburbPostcode.first, user: User.first}, {street_number: 28, street_name: 'Wivenhoe place', suburb_postcode: SuburbPostcode.second, user: User.second}, {street_number: 16, street_name: 'Manning street', suburb_postcode: SuburbPostcode.third, user: User.third}])
+
+Property.create!([{name: 'Loved place', description: 'Fully stocked', property_type: PropertyType.second, property_location: PropertyLocation.second, bedroom_count: 2, bed_count: 2, bathroom_count: 2, availability: true, start_date: 01-01-2020, end_date: 01-01-2023, nightly_price: 50}, {name: 'Comfort Unit', description: 'Comfort Unit to stay', property_type: PropertyType.third, property_location: PropertyLocation.third, bedroom_count: 3, bed_count: 3, bathroom_count: 2, availability: true, start_date: 01-01-2020, end_date: 01-01-2023, nightly_price: 60},])
+
+Booking.create!([{check_in_date: 03-05-2021, check_out_date: 05-05-2021, adult_count: 2, child_count: 0, traveller: User.first, host: User.second, property: Property.first}])
