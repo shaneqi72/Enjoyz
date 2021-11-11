6d806d9bf1b8a892c0fca571aad486971ebd6bd9
shane <shane_qi@hotmail.com>
2021-11-11 13:45:36 +1000

    update profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,5 @@
 class Profile < ApplicationRecord
-  validates :first_name, :last_name, :contact_number, presence: true
+  validates :first_name, :last_name, presence: true
+  validates :contact_number, presence: true, numericality: true
   belongs_to :user
 end
